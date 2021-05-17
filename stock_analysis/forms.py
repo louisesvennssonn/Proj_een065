e94c487a21fc3a42d57f6e8f27fb71ab65bcf600
Louise Svensson <louisesvennssonn@gmail.com>
2021-05-17 22:52:50 +0200

    Update 1

--- stock_analysis/forms.py
+++ stock_analysis/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FloatField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FloatField, IntegerField, DateTimeField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from stock_analysis.models import User, Stock, Analysis
@@ -75,12 +75,23 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
 
+
+class StockForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=1, max= 20)])
+    number_of_shares = IntegerField('Number of shares', validators=[DataRequired()])
+    create = SubmitField('Create')
+
+
 class AnalysisForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=50)])
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=50)])
     content = TextAreaField('Content', validators=[DataRequired()])
     price = FloatField('Price', validators=[DataRequired()])
     earnings = FloatField('Earnings', validators=[DataRequired()])
-    p_e = FloatField('p_e', validators=[DataRequired(),])
-    market_cap= FloatField('Market_cap', validators=[DataRequired()], default=Stock.number_of_shares*price)
+    p_e = FloatField('p_e', validators=[DataRequired()])
+    market_cap= FloatField('Market cap', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+
+
 # TODO: create here your forms
--- stock_analysis/routes.py
+++ stock_analysis/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from stock_analysis import app, db, bcrypt
-from stock_analysis.forms import RegistrationForm, LoginForm, UpdateAccountForm, AnalysisForm
+from stock_analysis.forms import RegistrationForm, LoginForm, UpdateAccountForm, AnalysisForm, StockForm
 from stock_analysis.models import User, Analysis, Stock, Diagram
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -12,8 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 def home():
     analyses = []
     if current_user.is_authenticated:
-        analyses = Analysis.query
-        return render_template('home.html', analyses=analyses)
+        analyses = Analysis.query.filter_by(user_id=current_user.id).all()
+    return render_template('home.html', analyses=analyses)
 
 
 @app.route("/about")
@@ -136,33 +136,134 @@ def account():
                            form=form)
 
 
-@app.route("/analysis/new", methods=['GET', 'POST'])
+@app.route("/stock/new", methods=['GET', 'POST'])
 @login_required
-def new_analysis():
+def create_stock():
+    form = StockForm()
+    if form.validate_on_submit():
+        created_stock = Stock(name=form.name.data,
+                            number_of_shares=form.content_type.data,
+                            )
+        db.session.add(created_stock)
+        db.session.commit()
+        flash('Your stock has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_stock.html',
+                           title='New Stock',
+                           form=form,
+                           legend='New Stock')
+
+
+@app.route("/stock/<str:stock_id>", methods=['GET', 'POST'])
+def stock(stock_id):
+    current_stock = Stock.query.get_or_404(stock_id)
+    form = AnalysisForm
+    if form.validate_on_submit():
+        if current_user.is_authenticated:
+            new_analysis = Analysis(title=form.title.data,
+                                    content=form.content.data,
+                                    price=form.price.data,
+                                    earnings=form.earnings.data,
+                                    p_e=form.price / form.earnings,
+                                    market_cap=form.price * Stock.number_of_shares
+                                    )
+            db.session.add(new_analysis)
+            db.session.commit()
+            flash('Your analysis has been submitted!', 'success')
+            return redirect(f'/stock/{current_stock.id}')
+        else:
+            flash('You are not logged in. You need to be logged in to be able to create an analysis!', 'danger')
+    return render_template('stock.html',
+                           name=current_stock.name,
+                           stock=current_stock,
+                           form=form)
+
+
+@app.route("/stock/<int:stock_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_stock(stock_id):
+    stock_to_update = Stock.query.get_or_404(stock_id)
+    if stock_to_update.author != current_user:
+        abort(403)  # only the owner of the post can edit it!
+    form = StockForm()
+    if form.validate_on_submit():
+        stock_to_update.name = form.name.data
+        stock_to_update.number_of_shares = form.number_of_shares.data
+        db.session.commit()
+        flash('Your stock has been updated!', 'success')
+        return redirect(url_for('post', post_id=stock_to_update.id))
+    elif request.method == 'GET':
+        form.name.data = stock_to_update.name
+        form.number_of_shares.data = stock_to_update.number_of_shares
+    return render_template('create_stock.html',
+                           title='Update Stock',
+                           form=form,
+                           legend='Update Stock')
+
+
+@app.route("/stock/<str:stock_id>/delete", methods=['POST'])
+@login_required
+def delete_post(stock_id):
+    stock_to_delete = Stock.query.get_or_404(stock_id)
+    if stock_to_delete.author != current_user:
+        abort(403)  # only the author can delete their posts
+    # first we need to delete all the comments
+    # this can be also configured as "cascade delete all"
+    # so that all comments are deleted automatically
+    # I personally prefer explicitly deleting the child rows
+    # see models.py file, class Comment
+    for your_stock in stock_to_delete.stocks:
+        db.session.delete(your_stock)
+    db.session.delete(stock_to_delete)
+    db.session.commit()
+    flash('Your stock has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/stock/<int:analysis_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_analysis(analysis_id):
+    analysis_to_update = Analysis.query.get_or_405(analysis_id)
+    if analysis_to_update.author != current_user:
+        abort(403)  # only the owner of the post can edit it!
     form = AnalysisForm()
     if form.validate_on_submit():
-        created_analysis = Analysis(
-            title=form.title.data,
-            price=form.price.data,
-            earnings=form.earnings.data,
-            p_e=form.price / form.earnings,
-            market_cap=form.price * Stock.number_of_shares,
-            content=form.content.data,
-            user=current_user
-        )
-        db.session.add(created_analysis)
-        try:
-            db.session.commit()
-            flash('Your analysis has been created!', 'success')
-            return redirect(url_for('home'))
-        except Exception as e:
-            db.session.rollback()
-            app.logger.critical(f'Error while inserting a new analysis: {created_analysis}')
-            app.logger.exception(e)
-            flash('There was an error while creating your analysis. Try again later.', 'danger')
+        analysis_to_update.title = form.title.data
+        analysis_to_update.content = form.content.data
+        analysis_to_update.price = form.price.data
+        analysis_to_update.earnings = form.earnings.data
+        db.session.commit()
+        flash('Your analysis has been updated!', 'success')
+        return redirect(url_for('analysis', analysis_id=analysis_to_update.id))
+    elif request.method == 'GET':
+        form.title.data = analysis_to_update.title
+        form.content.data = analysis_to_update.content
+        form.price.data = analysis_to_update.price
+        form.earnings.data = analysis_to_update.earnings
+
     return render_template('create_analysis.html',
-                           title='New Analysis',
+                           title='Update Analysis',
                            form=form,
-                           legend='New Analysis')
+                           legend='Update Analysis')
+
+
+@app.route("/stock/<str:analysis_id>/delete", methods=['POST'])
+@login_required
+def delete_analysis(analysis_id):
+    analysis_to_delete = Stock.query.get_or_404(analysis_id)
+    if analysis_to_delete.author != current_user:
+        abort(403)  # only the author can delete their posts
+    # first we need to delete all the comments
+    # this can be also configured as "cascade delete all"
+    # so that all comments are deleted automatically
+    # I personally prefer explicitly deleting the child rows
+    # see models.py file, class Comment
+    for your_analysis in analysis_to_delete.analyses:
+        db.session.delete(your_analysis)
+    db.session.delete(analysis_to_delete)
+    db.session.commit()
+    flash('Your analysis has been deleted!', 'success')
+    return redirect(url_for('home'))
+
 
 # TODO: create here your routes
