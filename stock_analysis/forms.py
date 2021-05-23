784871a51629442d32c9fcda3e3dcc9c7ab1add0
Louise Svensson <louisesvennssonn@gmail.com>
2021-05-23 11:44:09 +0200

    Changes by macbook

--- stock_analysis/forms.py
+++ stock_analysis/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FloatField, IntegerField, DateTimeField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FloatField, IntegerField, DateTimeField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from stock_analysis.models import User, Stock, Analysis
+
 
 # WTForms fields: https://wtforms.readthedocs.io/en/2.3.x/fields/
 # WTForms HTML5 fields: https://wtforms.readthedocs.io/en/2.3.x/fields/#module-wtforms.fields.html5
